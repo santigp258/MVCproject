f28a1b1afb7882bd08c667b5577d888cba44eb0c
Santiago <santigp258@gmail.com>
2021-02-04 06:20:56 -0500

    Users Class

--- users/models/Users.php
+++ users/models/Users.php
@@ -1,5 +1,62 @@
 <?php
+require_once('../../connection.php');
+session_start();
+class Users extends Connection
+{
+    public function __construct()
+    {
+        $this->db = parent::__construct();
+    }
 
-class Users{
-    
+    public function login($user, $password)
+    {
+        $row = null;
+
+        $stmt = $this->db->prepare('SELECT * FROM users WHERE user=:user AND password=:password');
+        $stmt->bindparam('user', $user, PDO::PARAM_STR);
+        $stmt->bindparam('password', $password, PDO::PARAM_STR);
+        $stmt->execute();
+        if ($stmt->rowCount() == 1) {
+            $result = $stmt->fetch();
+            $_SESSION['id'] = $result['id_user'];
+            $_SESSION['name'] = $result['name'] . $result['lastname'];
+            $_SESSION['profile'] = $result['profile'];
+            return true;
+        }
+
+        return false;
+    }
+
+    public function getId()
+    {
+        return $_SESSION['id'];
+    }
+
+    public function getName()
+    {
+        return $_SESSION['name'];
+    }
+
+    public function getProfile()
+    {
+        return $_SESSION['profile'];
+    }
+
+    public function validateSession()
+    {
+        if ($_SESSION['id'] === null) {
+            header('Location: ../../index.php');
+        }
+    }
+
+    public function validateAdministratorSession()
+    {
+        if ($_SESSION['id'] !== null) {
+            if ($_SESSION['profile'] == 'teacher') {
+                header('Location: ../../students/views/index.php');
+            } else {
+                header('Location: ../../index.php');
+            }
+        }
+    }
 }
