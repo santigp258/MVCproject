64f84c663b5384c0ee320b380cf64b210e3997b5
Santiago <santigp258@gmail.com>
2021-02-04 05:33:55 -0500

    subjects wiews

---
+++ subjects/views/edit.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Edit Subject</title>
+</head>
+<?php include_once('../../includes/cdn.php') ?>
+
+<body>
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-4">Edit Subject</h1>
+        </div>
+        <form method="POST" action="../controllers/AddController.php">
+            <div class="form-group">
+                <label for="name">Name:</label>
+                <input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
+            </div>
+
+            <button type="submit" class="btn btn-primary mt-4">Edit</button>
+        </form>
+    </div>
+</body>
+
+</html>
---
+++ subjects/views/index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Notes System -Subjects</title>
+</head>
+<?php include_once('../../includes/cdn.php') ?>
+
+<body>
+
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-4">Subjects</h1>
+            <p class="lead">
+                <a class="btn btn-primary" href="add.php" role="button">Add Subject <i class="fas fa-plus-circle"></i></a>
+            </p>
+        </div>
+        <table class="table table-dark table-striped">
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Actions</th>
+            </tr>
+            <tr>
+                <td>1</td>
+                <td>Math</td>
+                <td>
+                    <a href="edit.php"><i class="far fa-edit"></i></a>
+                    <a href="delete.php"><i class="far fa-trash-alt"></i></a>
+                </td>
+            </tr>
+        </table>
+    </div>
+</body>
+
+</html>
