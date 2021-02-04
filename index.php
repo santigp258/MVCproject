fe6430661e196eaeb717e727094fd45fdbecb18a
Santiago <santigp258@gmail.com>
2021-02-04 05:05:31 -0500

    basic login styles

--- index.php
+++ index.php
@@ -6,22 +6,30 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Notes Sytem</title>
 </head>
+<?php include_once('./includes/cdn.php') ?>
+<style>
+    .personalizated {
+        width: 40%;
+        margin: 0 auto;
+        margin-top: 10%;
+    }
+</style>
 
 <body>
-    <h1>Login</h1>
-
-    <form action="users/controllers/LoginController.php" method="POST">
-
-        <label for="user">User</label>
-        <input type="text" name="user" id="user" required="" autocomplete="off" placeholder="User">
-        <br>
-        <br>
-        <label for="password">Password</label>
-        <input type="text" name="password" id="password" required="" placeholder="Password">
-        <br><br>
-
-        <input type="submit" value="Login">
-    </form>
+    <div class="personalizated">
+        <form action="/examples/actions/confirmation.php" method="post">
+            <h2 class="text-center">Log in</h2>
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="User" id="user" name="user" required="required">
+            </div>
+            <div class="form-group mt-3">
+                <input type="password" class="form-control" id="password" name="password" placeholder="Password" required="required">
+            </div>
+            <div class="col text-center">
+                <button type="submit" class="btn btn-primary mt-2 w-50">Log in</button>
+            </div>
+        </form>
+    </div>
 </body>
 
 </html>
